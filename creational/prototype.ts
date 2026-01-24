3c8c7ad0842b7bfb58ec9308b3eda35f124cb264
Artem Sdobnikov <artem.sdobnikov25@gmail.com>
2026-01-24 19:07:27 +0200

    feat: implement prototype pattern

---
+++ creational/prototype.ts
@@ -0,0 +1,17 @@
+interface Cloneable<T> {
+  clone(): T;
+}
+
+class Pagination implements Cloneable<Pagination> {
+  constructor(
+    public page: number,
+    public pageSize: number,
+  ) {}
+
+  clone(overrides?: Partial<Pagination>): Pagination {
+    return new Pagination(
+      overrides?.page ?? this.page,
+      overrides?.pageSize ?? this.pageSize,
+    );
+  }
+}
